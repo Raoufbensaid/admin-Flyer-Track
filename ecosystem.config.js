552ce4030b19248382d7af2a9aa047218af22b50
unknown <RaoufBensaid>
2025-03-15 04:31:25 +0100

    configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,13 @@ module.exports = {
       name: "admin-interface",
       script: "node_modules/next/dist/bin/next",
       args: "start",
-      cwd: "D:/FlyerTrack/admin-interface",
+      cwd: "/root/admin-Flyer-Track", // adapte ce chemin si nécessaire
       interpreter: "node",
       exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001, // Choisis le port que tu veux utiliser
+      },
     },
   ],
 };
