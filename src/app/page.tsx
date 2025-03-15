ee15b6e6e217dab40d52b2594a24eb6115834828
unknown <RaoufBensaid>
2025-03-15 23:39:38 +0100

    test deploy 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1>FlyerTrack</h1>
       <h1>Dashboard Admin</h1>
-      <p>Interface administration FlyerTrack</p>
+      <p>Interface administration Auth</p>
     </div>
   );
 }
